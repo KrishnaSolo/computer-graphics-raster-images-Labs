8706b2313ac02609182eacf75683884af7f5c038
KrishnaSolo <krishna.solanki@mail.utoronto.ca>
2021-01-27 23:14:23 -0500

    First lab - converting rgba to rgb

--- src/rgba_to_rgb.cpp
+++ src/rgba_to_rgb.cpp
@@ -1,4 +1,5 @@
 #include "rgba_to_rgb.h"
+#include <iostream>
 
 void rgba_to_rgb(
   const std::vector<unsigned char> & rgba,
@@ -10,4 +11,16 @@ void rgba_to_rgb(
   ////////////////////////////////////////////////////////////////////////////
   // Add your code here
   ////////////////////////////////////////////////////////////////////////////
+
+  // we need to iterate throught the array skipping every 4th element which represents alpha
+  for(int i = 0; i < height; ++i) {
+    for(int j = 0; j < width; ++j){
+      for(int k = 0; k < 4; ++k) {
+        if(k != 3) {
+          rgb[k + 3*(j + width*i)] = rgba[k + 4*(j + width*i)];
+        }
+      }
+    }
+  }
+
 }
