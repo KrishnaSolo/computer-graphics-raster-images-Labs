12457239261cc51d2c7e2f0297971bc10b9e7548
KrishnaSolo <krishna.solanki@mail.utoronto.ca>
2021-01-28 23:12:51 -0500

    Finished lab3 - rotations

--- src/reflect.cpp
+++ src/reflect.cpp
@@ -1,4 +1,5 @@
 #include "reflect.h"
+#include "iostream"
 
 void reflect(
   const std::vector<unsigned char> & input,
@@ -11,4 +12,26 @@ void reflect(
   ////////////////////////////////////////////////////////////////////////////
   // Add your code here
   ////////////////////////////////////////////////////////////////////////////
+
+  std::vector<std::vector<std::vector<unsigned char>>> temp(height);
+  for(int i = 0; i < height; ++i) {
+    std::vector<std::vector<unsigned char>> tmp(width);
+    for(int j = 0; j < width; ++j){
+      std::vector<unsigned char> t(num_channels);
+      for(int k = 0; k < 3; ++k) {
+        t[k] = input[k + 3*(j + width*i)];
+      }
+      tmp[j] = t;  
+    }
+    temp[i]=tmp;
+  }
+
+  for(int i = 0; i < temp.size(); ++i) {
+    std::reverse(temp[i].begin(), temp[i].end());
+    for(int j = 0; j < temp[i].size(); ++j){
+      for(int k = 0; k < temp[i][j].size(); ++k) {
+        reflected[k + 3*(j + width*i)] = temp[i][j][k];
+      }
+    }
+  }
 }
